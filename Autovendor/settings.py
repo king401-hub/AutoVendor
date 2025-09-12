afa0a3f500d9a44719220f356ee8f410a2f6e84e
risenmen-job <solomonenoch663@gmail.com>
2025-09-12 16:39:03 +0100

    sldnj

--- Autovendor/settings.py
+++ Autovendor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g^%i*^)+cje3a-4*pw@-w=c$1%b-#a#gux8rluk19r@zvg@n%*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
